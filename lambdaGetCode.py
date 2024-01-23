3441ed94de82f6ba6f3774c3a1c76590ca3ff370
samtweedie <76115945+samtweedie@users.noreply.github.com>
2024-01-23 16:30:46 +0000

    Updated lambda function code from AWS, now fully working. Gets atm or branch info within a given radius and returns to site in JSON. Takes params: lat, long, radius, table (atm or branch)

--- lambdaGetCode.py
+++ lambdaGetCode.py
@@ -2,21 +2,34 @@ import json
 import mysql.connector 
 
 def lambda_handler(event, context):
-    # TODO implement
+    requestData = event['queryStringParameters']
+    lat = requestData['lat']
+    long = requestData['long']
+    radius = requestData['radius']
+    table = requestData['table']
     cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',
                               host='bankdatabase.ct2cs466y605.us-east-1.rds.amazonaws.com',
                               database='BankDatabase')
     try:
         cursor = cnx.cursor()
-        cursor.execute("""
-          SELECT * FROM atm_data
-          WHERE atm_identification = 'A021461A'
-        """) #test query
+        query = """
+        SELECT *, ( 3959 * acos( cos( radians({Nlatitude}) ) * cos( radians( latitude) ) 
+        * cos( radians( longitude ) - radians({Nlongitude}) ) + sin( radians({Nlatitude}) ) * sin(radians(latitude)) ) ) AS distance 
+        FROM {Ntable}_data 
+        HAVING distance < {Ndistance}
+        ORDER BY distance ;
+        """.format(Nlatitude = lat, Nlongitude = long, Ndistance = radius, Ntable = table)
+        cursor.execute(query)
         result = cursor.fetchall()
+        
+        columnNames = [column[0] for column in cursor.description]
+        returnData = []
+        for row in result:
+            rowDict = dict(zip(columnNames, row))
+            returnData.append(rowDict)
     finally:
         cnx.close()
     return {
         'statusCode': 200,
-        'body': result #as a test returns all fields from database as json
+        'body': json.dumps(str(returnData))
     }
-
