1435b502ea7601f817d7ce76369cc97fd053e5b6
Cezary Sp <2449949@dundee.ac.uk>
2024-01-23 13:25:00 +0000

    fixed login css link

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="login.css">
     <title>Staff Login Page</title>
 </head>
 
