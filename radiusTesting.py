2830e098bd02d7ecab268a1e026c1d4dfec9bb15
samtweedie <76115945+samtweedie@users.noreply.github.com>
2024-01-23 10:55:07 +0000

    Update test
    Added function with params that will be passed in from website once integrated with api

--- radiusTesting.py
+++ radiusTesting.py
@@ -1,19 +1,22 @@
+import json
 import mysql.connector    
-cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',                      #connect to db
+
+def getAtms(lat, long, radius):      #lat long and radius will be passed in from website
+    cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',                      #connect to db
                               host='bankdatabase.ct2cs466y605.us-east-1.rds.amazonaws.com',
                               database='BankDatabase')
 
-try:
-   cursor = cnx.cursor()
-   cursor.execute("""
-      SELECT * FROM atm_data
+    try:
+    cursor = cnx.cursor()
+    cursor.execute("""
+        SELECT * FROM atm_data
 
 
 
 
 
-   """)                               #currently gets all atms
-   result = cursor.fetchall()
-   print (result)                    #print result
-finally:
-    cnx.close()
+    """)                               #currently gets all atms
+    result = cursor.fetchall()
+    print (result)                    #print result
+    finally:
+        cnx.close()
