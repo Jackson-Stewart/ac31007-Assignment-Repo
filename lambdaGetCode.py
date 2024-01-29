643d2f298df5696237088ba84fee09fa97fd3363
samtweedie <76115945+samtweedie@users.noreply.github.com>
2024-01-29 15:34:09 +0000

    Cleaned up some code and fixed comments for new sql

--- lambdaGetCode.py
+++ lambdaGetCode.py
@@ -11,16 +11,13 @@ def lambda_handler(event, context): #deault handler for lambda function
     cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',   #db login
                               host='bankdatabase.ct2cs466y605.us-east-1.rds.amazonaws.com',
                               database='BankDatabase')
-                              
+    
     atmFields = "atm_identification, brand_name, supported_languages, atm_services, accessibility, access_24_hours, supported_currencies, minimum_amount, note, other_accessibility_code, other_accessibility_name, other_accessibility_description, branch_identification, location_category, other_location_category_code, other_location_category_name, other_location_category_description, site_identification, site_name, street_name, town_name, country_subdivision, country, post_code, latitude, longitude"
     branchesFields = "branch_identification, brand_name, branch_name, branch_type, customer_segments, services_and_facilities, accessibility, opening_hours_monday, opening_hours_tuesday, opening_hours_wednesday, opening_hours_thursday, opening_hours_friday, opening_hours_saturday, opening_hours_sunday, contact_phone, street_name, town_name, country_subdivision, country, post_code, latitude, longitude"
     fields = branchesFields if table == "branches" else atmFields   #list of fields to select, either from atm or branches
     try:
         cursor = cnx.cursor()
         #SQL query to search database for either ATMs or branches in a given radius
-        #Works by finding distance to each, and selecting any with a distance of less than the radius
-        #Uses Haversine formula to find distance between 2 coordinates
-        #source: https://stackoverflow.com/questions/27708490/haversine-formula-definition-for-sql
         query = """
             SELECT {Nfields}
             FROM 
@@ -37,9 +34,7 @@ def lambda_handler(event, context): #deault handler for lambda function
         returnData = []
         #Iterates through each row of retreived data forming key value pairs of field names, and the data
         #These are then added to an array, giving an array of key value pairs ready to be  
-        for row in result:
-            rowDict = dict(zip(columnNames, row))
-            returnData.append(rowDict)
+        returnData = [dict(zip(columnNames, row)) for row in result]
     finally:
         cnx.close()
     return {
