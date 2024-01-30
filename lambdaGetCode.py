8b7c6fafe3ff984f20470563a0b12281f980bfee
timothybaxter <91533436+timothybaxter@users.noreply.github.com>
2024-01-30 13:24:28 +0000

    Added variable for filtering requests in the sql

--- lambdaGetCode.py
+++ lambdaGetCode.py
@@ -1,19 +1,21 @@
 import json
 import mysql.connector  #used to connect to mysql database, all dependencies are uploaded to aws
-
+ 
 def lambda_handler(event, context): #deault handler for lambda function
-    
+   
     requestData = event['queryStringParameters']    #gets parameters passed in through url
     lat = requestData['lat']    #coordinates to search from
     long = requestData['long']
     radius = requestData['radius']  #radius to search (miles)
     table = requestData['table']    #selects either "atm" or "branches" table
+    filter = requestData['filter']  #passes the filters from the website into the SQL
     cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',   #db login
                               host='bankdatabase.ct2cs466y605.us-east-1.rds.amazonaws.com',
                               database='BankDatabase')
-                              
+                             
     atmFields = "atm_identification, brand_name, supported_languages, atm_services, accessibility, access_24_hours, supported_currencies, minimum_amount, note, other_accessibility_code, other_accessibility_name, other_accessibility_description, branch_identification, location_category, other_location_category_code, other_location_category_name, other_location_category_description, site_identification, site_name, street_name, town_name, country_subdivision, country, post_code, latitude, longitude"
     branchesFields = "branch_identification, brand_name, branch_name, branch_type, customer_segments, services_and_facilities, accessibility, opening_hours_monday, opening_hours_tuesday, opening_hours_wednesday, opening_hours_thursday, opening_hours_friday, opening_hours_saturday, opening_hours_sunday, contact_phone, street_name, town_name, country_subdivision, country, post_code, latitude, longitude"
+    filter = 'AND ' + filter if filter != '' else None
     fields = branchesFields if table == "branches" else atmFields   #list of fields to select, either from atm or branches
     try:
         cursor = cnx.cursor()
@@ -22,21 +24,22 @@ def lambda_handler(event, context): #deault handler for lambda function
         #Uses Haversine formula to find distance between 2 coordinates
         #source: https://stackoverflow.com/questions/27708490/haversine-formula-definition-for-sql
         query = """
-        SELECT {Nfields}, ( 3959 * acos( cos( radians({Nlatitude}) ) * cos( radians( latitude) ) 
-        * cos( radians( longitude ) - radians({Nlongitude}) ) + sin( radians({Nlatitude}) ) * sin(radians(latitude)) ) ) AS distance 
-        FROM {Ntable}_data 
-        HAVING distance < {Ndistance}
-        ORDER BY distance ;
-        """.format(Nfields = fields, Nlatitude = lat, Nlongitude = long, Ndistance = radius, Ntable = table)
+            SELECT {Nfields}
+            FROM
+                {Ntable}_data
+            WHERE
+                ST_Distance_Sphere(
+                    geolocation,
+                    ST_GeomFromText('POINT({Nlongitude} {Nlatitude})') {Nfilter}
+                ) <= {Ndistance};
+        """.format(Nfields = fields, Nlatitude = lat, Nlongitude = long, Ndistance = radius, Ntable = table, Nfilter = filter)
         cursor.execute(query)
         result = cursor.fetchall()  #result stores all the values
         columnNames = [column[0] for column in cursor.description]  #columnNames stores all the field names
         returnData = []
         #Iterates through each row of retreived data forming key value pairs of field names, and the data
         #These are then added to an array, giving an array of key value pairs ready to be  
-        for row in result:
-            rowDict = dict(zip(columnNames, row))
-            returnData.append(rowDict)
+        returnData = [dict(zip(columnNames, row)) for row in result]
     finally:
         cnx.close()
     return {
--- radiusTesting.py
+++ radiusTesting.py
@@ -3,19 +3,24 @@ import mysql.connector
 
 def getAtms(lat, long, radius):      #lat long and radius will be passed in from website
     print("test")
-    cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',                      #connect to db
+    Filter = 'accessibility LIKE "%WheelchairAccessible%"'
+    filter = 'AND ' + Filter
+    cnx = mysql.connector.connect(user='admin', password='Group3MasterPassword!',   #db login
                               host='bankdatabase.ct2cs466y605.us-east-1.rds.amazonaws.com',
                               database='BankDatabase')
 
     try:
         cursor = cnx.cursor()
         cursor.execute("""
-            SELECT atm_identification, ( 3959 * acos( cos( radians(56.458510) ) * cos( radians( latitude) ) 
-            * cos( radians( longitude ) - radians(-2.982718) ) + sin( radians(56.458510) ) * sin(radians(latitude)) ) ) AS distance 
-            FROM atm_data 
-            HAVING distance < 5
-            ORDER BY distance ;
-            """)                               #currently gets all atms
+             SELECT *
+            FROM
+                branches_data
+            WHERE
+                ST_Distance_Sphere(
+                    geolocation,
+                    ST_GeomFromText('POINT(-4.083687, 52.415085)') 
+                ) <= 10000000;   
+            """.format(Nfilter = filter))                               #currently gets all atms
         result = cursor.fetchall()
         print("test")
         print (result)                    #print result
