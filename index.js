60c111055187e437e122d8c19cd6c2b2cf4bf66a
Uwais Hussein <uwais.hussein93@gmail.com>
2024-01-31 16:34:24 +0000

    amending api links

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function geoFindMe() {
     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
 
     //setting the current co-ordinates within the url
-    const apiUrl = `https://uq1fh77mk8.execute-api.us-east-1.amazonaws.com/production/res?lat=${latitude}&long=${longitude}&radius=${chosenRadius}&table=${locationType}filter=""`;
+    const apiUrl = `https://uq1fh77mk8.execute-api.us-east-1.amazonaws.com/production/res?lat=${latitude}&long=${longitude}&radius=${chosenRadius}&table=${locationType}&filter=""`;
     fetchData(apiUrl, latitude, longitude, true);
   }
 
@@ -166,7 +166,7 @@ function geoFindMe() {
   function usePresetLocation() {
     const presetLatitude = 51.505;
     const presetLongitude = -0.09;
-    const presetApiUrl = `https://uq1fh77mk8.execute-api.us-east-1.amazonaws.com/production/res?lat=${presetLatitude}&long=${presetLongitude}&radius=${chosenRadius}&table=${locationType}filter=""`;
+    const presetApiUrl = `https://uq1fh77mk8.execute-api.us-east-1.amazonaws.com/production/res?lat=${presetLatitude}&long=${presetLongitude}&radius=${chosenRadius}&table=${locationType}&filter=""`;
     fetchData(presetApiUrl, presetLatitude, presetLongitude);
 
     const firstLocation = document.querySelector("#branch-info").dataset.location;
@@ -248,7 +248,7 @@ function updateMapView() {
             const lat = parseFloat(result.lat);
             const lon = parseFloat(result.lon);
 
-            const apiUrl = `https://9o3co4oqce.execute-api.us-east-1.amazonaws.com/production/resources?lat=${lat}&long=${lon}&radius=${chosenRadius}&table=${locationType}filter=""`;
+            const apiUrl = `https://9o3co4oqce.execute-api.us-east-1.amazonaws.com/production/resources?lat=${lat}&long=${lon}&radius=${chosenRadius}&table=${locationType}&filter=""`;
 
             fetchData(apiUrl, lat, lon, false);
           } else {
@@ -273,7 +273,7 @@ function updateMapView() {
             const lat = parseFloat(result.lat);
             const lon = parseFloat(result.lon);
 
-            const apiUrl = `https://9o3co4oqce.execute-api.us-east-1.amazonaws.com/production/resources?lat=${lat}&long=${lon}&radius=${chosenRadius}&table=${locationType}filter=""`;
+            const apiUrl = `https://9o3co4oqce.execute-api.us-east-1.amazonaws.com/production/resources?lat=${lat}&long=${lon}&radius=${chosenRadius}&table=${locationType}&filter=""`;
 
             fetchData(apiUrl, lat, lon, false);
           } else {
